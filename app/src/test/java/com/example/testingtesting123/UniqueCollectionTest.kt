9812f64b103b4dffdb8229bacae3bc1d11706391
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2024-04-18 11:57:06 -0400

    Added tests for: - Getting an item from the collection - Removing an item in the collection - Removing an item from an empty collectoin - Removing an item NOT in the collection - Also I made the clear function function properly

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -41,4 +41,45 @@ class UniqueCollectionTest {
 
         assert(originalSize == 2 && newSize == 0) {"Items not cleared"}
     }
+
+    @Test
+    fun getItems(){
+        val item0 = "Aaaa"
+        val item1 = "Bbbb"
+        val item2 = "Cccc"
+        collection.addItem(Item(item0))
+        collection.addItem(Item(item1))
+        collection.addItem(Item(item2))
+
+        assert(collection.get(1) == Item(item1))
+        assert(collection.get(0) == Item(item0))
+        assert(collection.get(2) == Item(item2))
+    }
+
+    @Test
+    fun removeItems(){
+        val itemname = "Blue"
+        val itemname2 = "Red"
+        collection.addItem(Item(itemname))
+        collection.addItem(Item(itemname2))
+        collection.remove(Item(itemname))
+        assert(collection.get(0) == Item(itemname2))
+    }
+
+    @Test
+    fun removeItemsFromEmptyList(){
+        val item = "A"
+        collection.remove(Item(item))
+        assert(collection.size() == 0)
+    }
+    @Test
+    fun removeItemNotInList(){
+        val existingItem = "A"
+        val nonexistingItem = "B"
+        collection.addItem(Item(existingItem))
+        collection.remove(Item(nonexistingItem))
+        assert(collection.size() == 1)
+        assert(collection.get(0) == Item(existingItem))
+    }
+
 }
